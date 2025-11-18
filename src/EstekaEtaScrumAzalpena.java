debb9d9265c2655b8e4ad6470b1647bc71fc1b07
iker <ikimoreno24@lhusurbil.eus>
2025-11-18 09:01:26 +0100

    dirst commit

--- src/EstekaEtaScrumAzalpena.java
+++ src/EstekaEtaScrumAzalpena.java
@@ -5,6 +5,6 @@ public class EstekaEtaScrumAzalpena {
         System.out.println("Scrum metodologia Sprintak egiten dira, normalena 2-4 aste bitartekoetan, checklistak eta ariketak egiteko denbora finkoa ezartzen da; Sprintak kolore bateko etiketen bidez adierazten dira.");
         System.out.println("Gainera, zeinek egingo duen ariketa hori ere finkatzen da, beraz norbanakoaren ardura oso garrantzitsua da. Tableroan erabili dudan puntuazio metodoa fibonachi sistema da, baina beste batzuk ere badaude, adibidez; poker sistema.");
         System.out.println("Hau da tableroaren linka: https://trello.com/invite/b/691c1f2cd506c1ad67d0a14a/ATTI8737750a657e392e7c22124d91cbcf0dD029A692/azterketascrum");
-        System.out.println("GITHUB-eko esteka: https://github.com/ikimoreno24-cell/Garapen-Inguruneko-Azterketa/settings/access");
+        System.out.println("GITHUB-eko esteka: https://github.com/ikimoreno24-cell/ZunemaUsurbilekoAtazak/settings/access?guidance_task=");
     }
 }
