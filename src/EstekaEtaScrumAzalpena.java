e2d0fd0ef8e7d98bc5b0bfeb578060d8b47973a2
iker <ikimoreno24@lhusurbil.eus>
2025-11-18 08:58:13 +0100

    first commit

---
+++ src/EstekaEtaScrumAzalpena.java
@@ -0,0 +1,10 @@
+public class EstekaEtaScrumAzalpena {
+
+    public static void main(String[] args){
+        System.out.println("Scrum metodologia, metodologia agile bat da, Scrum master bat dago, daily Scrum gestionatzen duena, retrospective egiten dela zihurtatzen duena eta sprinten planifikazioa ondo egiten dela zihurtatzen duena da..");
+        System.out.println("Scrum metodologia Sprintak egiten dira, normalena 2-4 aste bitartekoetan, checklistak eta ariketak egiteko denbora finkoa ezartzen da; Sprintak kolore bateko etiketen bidez adierazten dira.");
+        System.out.println("Gainera, zeinek egingo duen ariketa hori ere finkatzen da, beraz norbanakoaren ardura oso garrantzitsua da. Tableroan erabili dudan puntuazio metodoa fibonachi sistema da, baina beste batzuk ere badaude, adibidez; poker sistema.");
+        System.out.println("Hau da tableroaren linka: https://trello.com/invite/b/691c1f2cd506c1ad67d0a14a/ATTI8737750a657e392e7c22124d91cbcf0dD029A692/azterketascrum");
+        System.out.println("GITHUB-eko esteka: https://github.com/ikimoreno24-cell/Garapen-Inguruneko-Azterketa/settings/access");
+    }
+}
